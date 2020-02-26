0069875b28fcd4528845bd0d2b4248555aff3d2e
Alan Panayotov <alanpanayotov@gmail.com>
2020-02-26 23:46:01 +0000

    add home,base page. add sass middleware

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
+const path = require('path')
+
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.static('public'))
+const sassMiddleware = require('node-sass-middleware')
+
+
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'src', 'stylesheets')
+ ,dest: path.join(__dirname, 'public', 'stylesheets')
+ ,debug: true
+ ,outputStyle: 'compressed'
+ ,prefix: "/stylesheets"
+ ,indentedSyntax: true
+}))
 app.set('view engine', 'pug')
+app.use(express.static('public', { index: false }))
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+  res.render('index', { title: 'Hey', time: new Date(Date.now()) })
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}.`))
